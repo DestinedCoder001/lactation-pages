e423945a47855e1a3bc5132c4f132770d35f9ae8
Olowokere_Destiny <destinyolowokere@gmail.com>
2024-09-16 12:39:56 +0100

    initial all issues fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import "@/styles/globals.css";
+import "../assets/styles/carousel.css";
+import "../components/widgets/Sidebar.css";
+import "../components/widgets/Weaning.css";
+
 import type { AppProps } from "next/app";
+import RecoilContextProvider from "@/components/common/RecoilContextProvider";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <RecoilContextProvider>
+      <Component {...pageProps} />
+    </RecoilContextProvider>
+  );
 }
